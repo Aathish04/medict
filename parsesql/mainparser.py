4973c653b8f73327a4828b78f8970d82ebb0a182
Syrus Dark <naveen@syrusdark.website>
2020-06-15 17:29:57 +0530

    Working on SQL table layout

--- parsesql/mainparser.py
+++ parsesql/mainparser.py
@@ -7,7 +7,7 @@ import json
 import mysql.connector as ms
 from os import path
 
-from .constants import mySqlDatabase,mySqlPassword,mySqlHost,mySqlUsername,SQLTableName
+from constants import mySqlDatabase,mySqlPassword,mySqlHost,mySqlUsername,SQLTableName
 
 
 def parseCSV(fp):
@@ -18,14 +18,14 @@ def parseCSV(fp):
             rowsList.append(rows)
     return rowsList
 
-def isNum(var):
+def is_num(var):
     try:
         var=int(var)
         return True
     except ValueError:
         return False
 
-def intialiseDataBase():
+def intialise_dataBase():
     '''Initialises all the variable required for qurying MySql'''
     global con,cursor
     con=ms.connect(
@@ -35,6 +35,17 @@ def intialiseDataBase():
         database=mySqlDatabase
     )
     cursor=con.cursor()
+
+def checkTableExists(tablename='patients'):
+    intialise_dataBase()
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM information_schema.tables
+        WHERE table_name = '{0}'
+        """.format(tablename.replace('\'', '\'\'')))
+    if cursor.fetchone()[0] == 1:
+        return True
+    return False
 
 def sql_to_list():
     '''Converts things in sql database to lists which contain rows as tuples
@@ -52,22 +63,36 @@ def sql_to_list():
         (10, 57, 'MALE', '["DYSPNEA, DRY COUGH, FEVER"]', '["7, 7, 7"]', Decimal('38.50'), 'MULTI-DRUG THERAPY, O2-THERAPY', None)
     ]
     '''
-    intialiseDataBase()
+    intialise_dataBase()
     cursor.execute("SELECT * from %s"%SQLTableName)
     return cursor.fetchall()
-def writeDatabase(columnNames,rows,tableName):
+
+def create_table():
+    intialise_dataBase()
+    with open(path.join('..','database.sql'),'r') as f:
+        SQLStatement=f.read()
+    cursor.execute(SQLStatement)
+
+def show_table_rows():
+    '''This function would return the column names in SQL Tables.'''
+    intialise_dataBase()
+    cursor.execute("DESC %s"%SQLTableName)
+    desc = cursor.fetchall()
+    return [i[0] for i in desc]
+    
+def write_database(columnNames,rows):
     '''Write into MySql table name as specified.
     columnNames -> List
     rows ->List
     tablesName ->str
     '''
-    intialiseDataBase()
+    intialise_dataBase()
     sympIndex=columnNames.index('SYMPTOMS')
     timesIndex=columnNames.index('TIMES')
     for row in rows[1:]: #1st row in columnName
         row[sympIndex]=json.dumps([row[sympIndex]])
         row[timesIndex]=json.dumps([row[timesIndex]])
-        SQLStatement="INSERT INTO %s("%tableName
+        SQLStatement="INSERT INTO %s("%SQLTableName
         for i in range(len(columnNames)):
             if i!=len(columnNames)-1:
                 SQLStatement+=columnNames[i]+','
@@ -75,18 +100,25 @@ def writeDatabase(columnNames,rows,tableName):
                 SQLStatement+=columnNames[i]+')'
         SQLStatement+=" VALUES("
         for i in range(len(row)):
-            if i!=(len(row)-1) and isNum(row[i]):
+            if i!=(len(row)-1) and is_num(row[i]):
                 SQLStatement+=row[i]+','
-            elif i!=(len(row)-1) and (not isNum(row[i])):
+            elif i!=(len(row)-1) and (not is_num(row[i])):
                 SQLStatement+="'%s'"%row[i]+','
-            elif i==(len(row)-1) and isNum(row[i]):
+            elif i==(len(row)-1) and is_num(row[i]):
                 SQLStatement+=row[i]+');'
-            elif i==(len(row)-1) and (not isNum(row[i])):
+            elif i==(len(row)-1) and (not is_num(row[i])):
                 SQLStatement+="'%s'"%row[i]+');'
         print(SQLStatement)
-        cursor.execute(SQLStatement)
+        if checkTableExists()==True:
+            cursor.execute(SQLStatement)
+        else:
+            create_table()
+            cursor.execute(SQLStatement)
         con.commit()
+
 if __name__== '__main__':
-    #a=parseCSV(path.join(".","data.csv"))
-    #print(writeDatabase(tuple(a[0]),tuple(a),SQLTableName))
+    a=parseCSV(path.join("..","data.csv"))
+    print(write_database(tuple(a[0]),tuple(a)))
     for i in sql_to_list(): print(i)
+    #write_database(columnNames,rows)
+    print(show_table_rows())
