904f59324adf9ba28e59ae550ae30b32fa717078
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-06-16 10:29:25 +0530

    Add main Medict.py file. Medict.py will contain the main user interface.

---
+++ medict.py
@@ -0,0 +1,69 @@
+import os
+import csv
+import PySimpleGUI as sg
+from csvwriter import CSVManager
+
+if __name__=="__main__":
+    sg.theme('DarkTanBlue')
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+    csvmanager=CSVManager()
+
+    info_layout=[[sg.Text(csvmanager.INSTRUCTIONS,font=(csvmanager.TEXTFONT,12))]]
+    layout=[ # Main Window layout
+        [
+            sg.TabGroup(
+                [
+                    [
+                        sg.Tab("Info",info_layout),
+
+                        sg.Tab(
+                            "CSV Spreadsheet",csvmanager.spread_layout,
+                            element_justification='center'
+                            )
+                        ]
+                    ],
+                enable_events=True,key="tab"
+                )
+            ]
+        ]
+
+    window = sg.Window("Medict", layout).Finalize()
+    window.Maximize()
+
+    while True: #Main event loop.
+        event, values = window.read()
+
+        if event in (None, 'Exit'):
+            break
+
+        elif event=="tab":
+            csvmanager.clear_data(locals())
+            csvmanager.reload_table()
+
+        elif event=="table": #Table is clicked etc.
+            row=csvmanager.table.SelectedRows[-1]
+            for i in range(len(values.keys())):
+                key = list(values.keys())[i]
+                if key in list(csvmanager.FIELDS):
+                    window[key](csvmanager.table.get()[row][i-1])
+
+        elif event=="SUBMIT":
+            csvmanager.submit_filled(locals())
+
+        elif event=="RELOAD":
+            csvmanager.table.update(values=csvmanager.records_from_csv())
+
+        elif event=="CLEAR FILLED":
+            csvmanager.clear_data(locals())
+
+        elif event=="DELETE ROWS":
+            csvmanager.delete_selected_rows()
+
+        elif event == "DELETE ALL ROWS":
+            csvmanager.delete_all_rows()
+
+        else:
+            print(event)
+
+    window.close()
