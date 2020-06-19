296e7be80fbbe41f28e6c4eae0a6b61f513d217d
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-06-19 16:55:48 +0530

    Allow for fullscreen; enable resizing.
    Expand tabgroup in CSVWriter.

--- medict.py
+++ medict.py
@@ -7,14 +7,6 @@ except ModuleNotFoundError:
 
 from csvmanager import CSVManager
 
-def get_screen_size():
-    tempwindow=sg.Window("Medict", [[]],resizable=True,finalize=True)
-    tempwindow.Maximize()
-    size=tempwindow.size
-    tempwindow.close()
-    del tempwindow
-    return size
-
 if __name__=="__main__":
     sg.theme('DarkTanBlue')
     os.chdir(os.path.abspath(os.path.dirname(__file__)))
@@ -40,7 +32,8 @@ if __name__=="__main__":
             ]
         ]
 
-    window = sg.Window("Medict", layout,resizable=False,finalize=True,size=get_screen_size())
+    window = sg.Window("Medict", layout,resizable=True,finalize=True)
+    window.maximize()
     window["tab"].expand(True,True,True)
 
     while True: #Main event loop.
