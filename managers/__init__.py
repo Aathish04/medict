7565803d9a326ff8f2e6448a8ef18fc92c3aec5e
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-06-19 22:47:57 +0530

    Made this branch backwards compatible with master.
    I think this should be merged ASAP.

---
+++ managers/__init__.py
@@ -0,0 +1,4 @@
+from .csvmanager import CSVManager
+
+# from .sqlview import SQLManager # I just commented it out for now since my Mac doesn't have MySQL installed.
+# When testing, please uncomment this.
