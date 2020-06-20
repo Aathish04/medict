f9fe682041f0722728cab4c22f2f20d14656d842
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-06-20 12:21:46 +0530

    Removed an explanatory comment.

--- managers/__init__.py
+++ managers/__init__.py
@@ -1,4 +1,3 @@
 from .csvmanager import CSVManager
 
 # from .sqlview import SQLManager # I just commented it out for now since my Mac doesn't have MySQL installed.
-# When testing, please uncomment this.
