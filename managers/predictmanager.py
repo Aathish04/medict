873cc3644575b279b5425e608669a23f8801815e
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-06-25 18:47:31 +0530

    Made a Model and tested it's accuracy.

--- managers/predictmanager.py
+++ managers/predictmanager.py
@@ -1,93 +1,85 @@
 from os import path
 import numpy as np
+import pandas as pd
+
 import tensorflow as tf
-import functools
+
+from tensorflow import feature_column
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
 
 if __name__=="__main__":
     from csvmanager import CSVManager # PLS DO ALL IMPORTS SIMILARLY
 else:
     from .csvmanager import CSVManager
+
 csvmanager=CSVManager(CSVFILE=path.abspath(__file__ + "/../../data.csv"))
-
-class PackNumericFeatures(object):
-  def __init__(self, names):
-    self.names = names
-
-  def __call__(self, features, labels):
-    numeric_features = [features.pop(name) for name in self.names]
-    numeric_features = [tf.cast(feat, tf.float32) for feat in numeric_features]
-    numeric_features = tf.stack(numeric_features, axis=-1)
-    features['numeric'] = numeric_features
-
-    return features, labels
-
-def show_batch(dataset):
-  for batch, label in dataset.take(1):
-    for key, value in batch.items():
-      print("{:20s}: {}".format(key,value.numpy()))
-
-def normalize_numeric_data(data, mean, std):
-    return (data-mean)/std
-
 expanded_dataset=csvmanager.expanded_dataset()
 expanded_csv_path=path.abspath(__file__ + "/../expandeddata.csv")
 csvmanager.write_list_od_to_csv(expanded_dataset,datafile=expanded_csv_path)
 
-raw_train_data = tf.data.experimental.make_csv_dataset(
-      expanded_csv_path,
-      batch_size=len(csvmanager.records_from_csv()),
-      label_name="MORTALITY",
-      na_value="UNKNOWN",
-      num_epochs=1,
-      ignore_errors=True)
+dataframe=pd.read_csv(expanded_csv_path)
+dataframe.columns = dataframe.columns.str.strip().str.replace(' ', '-').str.replace('(', '').str.replace(')', '')
+train, test = train_test_split(dataframe, test_size=0.2)
+train, val = train_test_split(train, test_size=0.2)
 
-raw_test_data = tf.data.experimental.make_csv_dataset(
-      expanded_csv_path,
-      batch_size=len(csvmanager.records_from_csv()),
-      label_name="MORTALITY",
-      na_value="UNKNOWN",
-      num_epochs=1,
-      ignore_errors=True)
+print(len(train), 'train examples')
+print(len(val), 'validation examples')
+print(len(test), 'test examples')
 
-NUMERIC_FEATURES = ['AGE','TEMPERATURE',"FEVER","CHEST TIGHTNESS","DYSPNEA",
-"COUGH","CHEST PAIN","DIARRHEA","FLU","HYPERPYREXIA","HEADACHE",
-"BODY WEAKNESS","APIRETIC","WHEEZING","HYPERPYROXIA","ASTHENIA",
-"VOMIT","APYRETIC","PARACETAMOL","AMOXICILLIN","MULTI-DRUG THERAPY",
-"O2 THERAPY","INTUBATION","CPAP THERAPY","ANTIVIRALS","LEVOFLOXACIN",
-"LACTIC FERMENTS","TOCILIZUMAB","CORTISONE","CEPHALOSPORIN","EBPM",
-"LEVOXACIN","ACCLOVIRN","CHOLECALCIFOREL","PLAQUENIL","RITONAVIR THERAPY",
+
+def df_to_dataset(dataframe, shuffle=True, batch_size=55):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop('MORTALITY')
+    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(dataframe))
+    ds = ds.batch(batch_size)
+    return ds
+
+batch_size = 55
+train_ds = df_to_dataset(train, batch_size=batch_size)
+val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+
+for feature_batch, label_batch in train_ds.take(1):
+    print('Every feature:', list(feature_batch.keys()))
+    print('A batch of ages:', feature_batch['AGE'])
+    print('A batch of targets:', label_batch )
+
+example_batch = next(iter(train_ds))[0]
+
+feature_columns=[feature_column.indicator_column(feature_column.categorical_column_with_vocabulary_list('GENDER', ['MALE', 'FEMALE']))]
+
+NUMERIC_FEATURES = ['AGE','TEMPERATURE',"FEVER","CHEST-TIGHTNESS","DYSPNEA",
+"COUGH","CHEST-PAIN","DIARRHEA","FLU","HYPERPYREXIA","HEADACHE",
+"BODY-WEAKNESS","APIRETIC","WHEEZING","HYPERPYROXIA","ASTHENIA",
+"VOMIT","APYRETIC","PARACETAMOL","AMOXICILLIN","MULTI-DRUG-THERAPY",
+"O2-THERAPY","INTUBATION","CPAP-THERAPY","ANTIVIRALS","LEVOFLOXACIN",
+"LACTIC-FERMENTS","TOCILIZUMAB","CORTISONE","CEPHALOSPORIN","EBPM",
+"LEVOXACIN","ACCLOVIRN","CHOLECALCIFOREL","PLAQUENIL","RITONAVIR-THERAPY",
 "HYDROXYCHLOROQUINE","CORTICOSTEROID","ALENOLOL","AZITHROMYCIN","ZITROMAX",
 "TACHIPIRINE","ANTIBIOTICS","TAZIOCINBIS"]
 
-packed_train_data = raw_train_data.map(
-    PackNumericFeatures(NUMERIC_FEATURES))
+for header in NUMERIC_FEATURES:
+    feature_columns.append(feature_column.numeric_column(header))
 
-packed_test_data = raw_test_data.map(
-    PackNumericFeatures(NUMERIC_FEATURES))
+feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
 
-example_batch, labels_batch = next(iter(raw_train_data))
+model = tf.keras.Sequential([
+  feature_layer,
+  layers.Dense(128, activation='relu'),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(1)
+])
 
-MEAN=[sum(example_batch[entry].numpy())/len(example_batch[entry].numpy())
-    for entry in example_batch if entry in NUMERIC_FEATURES]
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
-STD=[np.std(example_batch[entry].numpy())
-    for entry in example_batch if entry in NUMERIC_FEATURES]
-
-normalizer = functools.partial(normalize_numeric_data, mean=MEAN, std=STD)
-
-numeric_column = tf.feature_column.numeric_column('numeric', normalizer_fn=normalizer, shape=[len(NUMERIC_FEATURES)])
-
-numeric_columns = [numeric_column]
-
-CATEGORIES = {
-    'GENDER': ['MALE', 'FEMALE']
-}
-categorical_columns = []
-
-for feature, vocab in CATEGORIES.items():
-    cat_col = tf.feature_column.categorical_column_with_vocabulary_list(
-        key=feature, vocabulary_list=vocab)
-    categorical_columns.append(tf.feature_column.indicator_column(cat_col))
-
-preprocessing_layer = tf.keras.layers.DenseFeatures(categorical_columns+numeric_columns)
-
+model.fit(train_ds,
+          validation_data=val_ds,
+          epochs=55)
+loss, accuracy = model.evaluate(test_ds)
+print("Accuracy", accuracy)
